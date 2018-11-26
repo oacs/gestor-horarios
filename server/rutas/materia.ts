0ee411adf94e93736541efe24322dd4512cf7cb9
Eduardo <iguana0201@gmail.com>
2018-11-26 09:38:19 -0400

    Fixed materias

--- server/rutas/materia.ts
+++ server/rutas/materia.ts
@@ -4,52 +4,53 @@ import { Router } from 'express';
 const router = Router();
 
 /** Obtengo todos los registros
- * 
+ *
  */
 router.get('/', function (req, res) {
-    db.all('select * from materia', (err, row) => {
-        console.log(err);
-        // console.log(row);
-        res.send(row);
-    });
+  db.all('select * from materia', (err, row) => {
+    console.log(err);
+    // console.log(row);
+    res.send(row);
+  });
 });
 
 /**Obtengo un registro dado su id
- * 
+ *
  */
 router.get('/:id', function (req, res) {
 
-    db.get('select * from materia where id = ' + req.params.id, (err, row) => {
-        console.log(err);
-        // console.log(row);
-        res.send(row);
-    });
+  db.get('select * from materia where id = ' + req.params.id, (err, row) => {
+    console.log(err);
+    // console.log(row);
+    res.send(row);
+  });
 });
 
 /**Obtengo un registro dado un parametro
  * @param id int
  * @param nombre text
  * @return Materia[]
- * 
+ *
  */
 router.get('/getBy', function (req, res) {
-    let query = 'select * from materia where '
-    let vars = {id: 0 , nombre: ''}
-    if(req.params.id){
-        query+= ' id = $id'
-        vars.id = req.params.id
-    }
-    if(req.params.nombre){
-        query+= ' nombre = $nombre',
-        vars.nombre = req.params.nombre
-    }
-    if(query != 'select * from materia where')
-    db.get(query,vars, (err, row) => {
-        console.log(err);
-        // console.log(row);
-        res.send(row);
+  let query = 'select * from materia where ';
+  const vars = { id: 0, nombre: '' };
+  if (req.params.id) {
+    query += ' id = $id';
+    vars.id = req.params.id;
+  }
+  if (req.params.nombre) {
+    query += ' nombre = $nombre',
+      vars.nombre = req.params.nombre;
+  }
+  if (query !== 'select * from materia where') {
+    db.get(query, vars, (err, row) => {
+      console.log(err);
+      // console.log(row);
+      res.send(row);
     });
-    res.status(400).send('error parametros no valido: enum[nombre,id]')
+  }
+  res.status(400).send('error parametros no valido: enum[nombre,id]');
 });
 
 /**Elimino un registro dado su id
@@ -57,11 +58,11 @@ router.get('/getBy', function (req, res) {
  */
 router.delete('/:id', function (req, res) {
 
-    db.get('delete from materia where id = ' + req.params.id, (err, row) => {
-        console.log(err);
-        // console.log(row);
-        res.send(row);
-    });
+  db.get('delete from materia where id = ' + req.params.id, (err, row) => {
+    console.log(err);
+    // console.log(row);
+    res.send(row);
+  });
 });
 
 /** Actualizo un registro dado un id
@@ -69,25 +70,26 @@ router.delete('/:id', function (req, res) {
  * @body nombre text
  */
 router.put('/:id', function (req, res) {
-    let vars = {
-        id : req.params.id,
-        nombre : ''
-    }
-    let query = 'update materia set '
-    if (req.body.nombre){
-        vars.nombre = req.body.nombre
-        query+= ' nombre = $nombre'
-    } 
+  const vars = {
+    id: req.params.id,
+    nombre: ''
+  };
+  let query = 'update materia set ';
+  if (req.body.nombre) {
+    vars.nombre = req.body.nombre;
+    query += ' nombre = $nombre';
+  }
 
-    query+= ' where id = $id'
-    if(query != 'update materia set  where id = $id')
+  query += ' where id = $id';
+  if (query !== 'update materia set  where id = $id') {
     if (req.body.nombre) {
-        db.run(query, vars, info => {
-            console.log(info);
-            res.send(info);
-        });
+      db.run(query, vars, info => {
+        console.log(info);
+        res.send(info);
+      });
     }
-    res.status(400).send('error parametros no valido: enum[nombre,id]')
+  }
+  res.status(400).send('error parametros no valido: enum[nombre,id]');
 });
 
 /** Guardo un registro dado un id
@@ -95,13 +97,13 @@ router.put('/:id', function (req, res) {
  */
 router.post('/', function (req, res) {
 
-    // console.log(req.body);
-    db.run('insert into materia( nombre)  values ( $nombre)', {
-        $nombre: req.body.nombre
-    }, info => {
-        console.log(info);
-        res.send(info);
-    });
+  // console.log(req.body);
+  db.run('insert into materia( nombre)  values ( $nombre)', {
+    $nombre: req.body.nombre
+  }, info => {
+    console.log(info);
+    res.send(info);
+  });
 });
 
 module.exports = router;
