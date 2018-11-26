309d35d1f97be401e8225528093db1c8af6eb5df
Victor Prado <vmpradob@gmail.com>
2018-11-26 02:23:55 -0400

    arreglos a pensum y revision de profesor eliminado seccion

--- server/rutas/pensum.ts
+++ server/rutas/pensum.ts
@@ -3,6 +3,9 @@ import { Router } from 'express';
 
 const router = Router();
 
+/**Obtengo todos los registros
+ * 
+ */
 router.get('/', function (req, res) {
     db.all('select * from pensum', (err, row) => {
         console.log(err);
@@ -10,7 +13,9 @@ router.get('/', function (req, res) {
         res.send(row);
     });
 });
-
+/**Obtengo un registro dado su id
+ * @param id int
+ */
 router.get('/:id', function (req, res) {
 
     db.get('select * from pensum where id = ' + req.params.id, (err, row) => {
@@ -19,7 +24,9 @@ router.get('/:id', function (req, res) {
         res.send(row);
     });
 });
-
+/**Elimino un registro dado su id
+ * @param id int
+ */
 router.delete('/:id', function (req, res) {
 
     db.get('delete from pensum where id = ' + req.params.id, (err, row) => {
@@ -28,9 +35,11 @@ router.delete('/:id', function (req, res) {
         res.send(row);
     });
 });
-
+/**Obtengo todas las materias de un pensum
+ * @param id int
+ */
 router.get('/:id/materias', function (req, res) {
-    let query = 'select * from materia as m'
+    let query = 'select *, mxp.horas as horas, mxp.maxH as maxH from materia as m'
         +'inner join materia_x_pensum as mxp on mxp.id_materia = m.id'
         +'inner join pensum as p on p.id = mxp.id_pensum'
         +'where p.id = $id'
@@ -41,7 +50,9 @@ router.get('/:id/materias', function (req, res) {
             res.send(info);
         });
 });
-
+/**Obtengo todas los periodos de un pensum
+ * @param id int
+ */
 router.get('/:id/periodos', function (req, res) {
     let query = 'select * from periodo as per'
         +'inner join pensum as pem on per.id_pensum = pem.id'
@@ -53,7 +64,10 @@ router.get('/:id/periodos', function (req, res) {
             res.send(info);
         });
 });
-
+/**Actualizo un registro dado su id
+ * @param id int
+ * @body fecha text
+ */
 router.put('/:id', function (req, res) {
     if (req.body.fecha) {
         db.run('UPDATE pensum SET fecha = $fecha WHERE id = $id', {
@@ -65,20 +79,28 @@ router.put('/:id', function (req, res) {
         });
     }
 });
+/**Le agrega una nueva materia al pensum actual
+ * @param id
+ * @body id_pensum int
+ * @body id_materia int
+ * @body id_pensum int
+ * @body horas int
+ * @body maxH int
+ */
 router.post('/:id/materia', function (req, res) {
-    if (req.body.fecha) {
-        db.run('insert into materia_x_pensum (horas,horasM,id_pensum,id_materia) values ($horas,$horasM,$id_pensum,$id_materia)', {
-            $id_pensum: req.params.id,
-            $id_materia: req.body.id_materia,
-            $horas : req.body.horas,
-            $horasM : req.body.horasM
-        }, info => {
-            console.log(info);
-            res.send(info);
-        });
-    }
+    db.run('insert into materia_x_pensum (horas,horasM,id_pensum,id_materia) values ($horas,$horasM,$id_pensum,$id_materia)', {
+        $id_pensum: req.params.id,
+        $id_materia: req.body.id_materia,
+        $horas : req.body.horas,
+        $horasM : req.body.horasM
+    }, info => {
+        console.log(info);
+        res.send(info);
+    });
 });
-
+/** Inserta un nuevo regsitro
+ * @body fecha text 
+ */
 router.post('/', function (req, res) {
 
     console.log(req.body);
