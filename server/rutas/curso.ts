91f947cc8858edc4b7dd4e8f1f7644a793b00d0e
Victor Prado <vmpradob@gmail.com>
2018-11-26 01:40:16 -0400

    correcciones a curso

--- server/rutas/curso.ts
+++ server/rutas/curso.ts
@@ -13,28 +13,45 @@ router.get('/', function (req, res) {
 
 router.get('/getBy', function (req, res) {
     let query = 'select * from curso where '
-    if(req.params.id_profesor) query+= ' id_profesor ='+req.params.id_profesor +','
+    let vars ={id_profesor : 0,id_periodo : 0,id_materia: 0,seccion: '',id_pensum : 0, semestre : 0}
+    if(req.params.id_profesor){
+        vars.id_profesor = req.params.id_profesor
+        query+= ' id_profesor = $id_profesor ,'
+    } 
     
-    if(req.params.id_periodo) query+= ' id_periodo ='+req.params.id_periodo +','
+    if(req.params.id_periodo){
+        vars.id_periodo = req.params.id_periodo
+        query+= ' id_periodo = $id_periodo ,'
+    } 
 
-    if(req.params.id_materia) query+= ' id_materia ='+req.params.id_materia +','
+    if(req.params.id_materia){
+        vars.id_materia = req.params.id_materia
+        query+= ' id_materia = $id_materia ,'
+    } 
 
-    if(req.params.semestre) query+= ' semestre ='+req.params.semestre +','
-
-    if(req.params.seccion) query+= ' seccion ='+req.params.seccion +','
-
-    if(req.params.id_pensum) query+= ' id_pensum ='+req.params.id_pensum +','
-
-    if(req.params.horario) query+= ' horario ='+req.params.horario +','
+    if(req.params.semestre){
+        vars.semestre = req.params.semestre
+        query+= ' semestre = $semestre ,'
+    } 
     
+    if(req.params.seccion){
+        vars.seccion = req.params.seccion
+        query+= ' seccion = $seccion ,'
+    } 
+
+    if(req.params.id_pensum){
+        vars.id_pensum = req.params.id_pensum
+        query+= ' id_pensum = $id_pensum ,'    
+    } 
     query = query.substr(0,query.length-1)
+    
     if(query != 'select * from curso where')
-    db.get(query, (err, row) => {
+    db.get(query,vars, (err, row) => {
         console.log(err);
         // console.log(row);
         res.send(row);
     });
-    res.status(400).send('error parametros no valido: enum[nombre,id]')
+    res.status(400).send('error parametros no valido: enum[id_profesor,id_periodo,id_materia,semestre,seccion,id_pensum]')
 });
 
 router.delete('/:id', function (req, res) {
@@ -47,22 +64,38 @@ router.delete('/:id', function (req, res) {
 });
 
 router.put('/:id', function (req, res) {
-    let query = 'update curso set '
-    if (req.body.semestre) query+= ' semestre = '+req.body.semestre
-    if (req.body.seccion) query+= ' id_profesor = '+req.body.id_profesor
-    if (req.body.horario) query+= ' horario = '+req.body.horario
+    let vars = {
+        semestre : 0,
+        id_profesor :'',
+        horario : '',
+        id_materia : req.params.id_materia, 
+        id_pensum : req.params.id_pensum, 
+        id_periodo : req.params.id_periodo,
+        seccion : req.params.seccion
+    }
+    let query = 'update curso set'
+    if(req.body.semestre){
+        vars.semestre = req.body.semestre
+        query+= ' semestre = $semestre ,'
+    } 
+    
+    if(req.body.id_profesor){
+        vars.id_profesor = req.body.id_profesor
+        query+= ' id_profesor = $id_profesor ,'
+    }
+    if (req.body.horario){
+        vars.horario = req.body.horario
+        query+= ' horario = $horario ,'
+    } 
+    query = query.substr(0,query.length-1)
 
     query+= ' where id_periodo = $id_periodo and id_materia = $id_materia and id_pensum = $id_pensum and seccion = $seccion'
-    if(query != 'update curso set where  id_periodo = $id_periodo and id_materia = $id_materia and id_pensum = $id_pensum and seccion = $seccion')
-    if (req.body.nombre) {
-        db.run(query, {
-            $id: req.params.id,
-        }, info => {
-            console.log(info);
-            res.send(info);
-        });
-    }
-    res.status(400).send('error parametros no valido: enum[nombre,id]')
+    if(query != 'update curso set where id_periodo = $id_periodo and id_materia = $id_materia and id_pensum = $id_pensum and seccion = $seccion')
+    db.run(query,vars, info => {
+        console.log(info);
+        res.send(info);
+    });
+    res.status(400).send('error...')
 });
 
 router.post('/', function (req, res) {
