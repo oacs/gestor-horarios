e32dcf5d0d591e38f8fc1f6b0fa87d649f09d485
Oscar Castillejo <oacs1221@gmail.com>
2019-01-18 05:32:51 -0400

    Buscador y mostrar disponibilidad en profesores

--- src/app/profesores/views/profesores/profesores.component.ts
+++ src/app/profesores/views/profesores/profesores.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { ProfesorService, Profesor } from '../../../providers/profesor/profesor.service';
+import { ProfesorClass } from '../../../providers/algoritmo/profesorClass';
+import { BloqueHoras } from '../../../providers/algoritmo/BloqueHoras';
+import { Hora } from '../../../providers/algoritmo/enum';
 
 
 @Component({
@@ -10,19 +14,43 @@ import { FormControl, FormBuilder, Validators, FormGroup } from '@angular/forms'
 export class ProfesoresComponent implements OnInit {
 
   newProfesor: boolean;
+  public hora = Hora;
+  public buscador: FormGroup;
+  public profesorSeleccionado: ProfesorClass;
+  public profesores: Profesor[];
+  public auxProfesores: Profesor[];
   public profesorForm: FormGroup;
+  public horario: number[][];
 
-
-
-
-  constructor(private formModal: FormBuilder) { 
+  constructor(
+    private formModal: FormBuilder,
+    private profesorService: ProfesorService,
+    private fb: FormBuilder
+  ) {
+    this.buscador = this.fb.group({ texto: [''] });
     this.newProfesor = false;
-    
-
+    this.profesorSeleccionado = new ProfesorClass(0, '', '', '');
     this.profesorForm = this.formModal.group({
       nombre: ['', Validators.required],
       email: ['', Validators.required]
-      
+
+    });
+    this.profesorService.getProfesores().subscribe(profesores => {
+      this.profesores = profesores;
+      this.auxProfesores = profesores;
+    });
+
+    this.horario = [];
+    for (let i = 0; i < 7; i++) {
+      this.horario.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    }
+
+    this.buscador.get('texto').valueChanges.subscribe(cambio => {
+      if (cambio === '') {
+        this.profesores = this.auxProfesores;
+      } else {
+        this.profesores = this.auxProfesores.filter(profesor => profesor.nombre.indexOf(cambio) >= 0 ? true : false);
+      }
     });
   }
 
@@ -33,12 +61,31 @@ export class ProfesoresComponent implements OnInit {
   /*
     Muestra y oculta el modal para crear un nuevo profesor
   */
- showNewModal() {
-  if (this.newProfesor === true) {
-    this.newProfesor = false;
-  } else {
-    this.newProfesor = true;
+  showNewModal() {
+    if (this.newProfesor === true) {
+      this.newProfesor = false;
+    } else {
+      this.newProfesor = true;
+    }
   }
-}
+
+  public selectProfesor(profesor: Profesor) {
+    this.profesorSeleccionado = new ProfesorClass(profesor.id, profesor.nombre, profesor.correo, profesor.disp);
+    console.log(this.profesorSeleccionado);
+    this.clearHorario();
+    this.profesorSeleccionado.disponibilidad.forEach(bloque => {
+      for (let i = bloque.inicio; i !== bloque.fin + 1; i++) {
+        this.horario[bloque.dia][i] = bloque.prioridad;
+      }
+    });
+  }
+
+  public clearHorario(): void {
+    for (let i = 0; i < 7; i++) {
+      this.horario[i].forEach(bloque => {
+        bloque = 0;
+      });
+    }
+  }
 
 }
