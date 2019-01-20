e953ae84ebc9877f602526737061458fcd82204f
josean0509 <joseanzacarias@gmail.com>
2019-01-20 18:23:28 -0400

    Profesores vista adaptada (falta cargar achivos)

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -6,6 +6,6 @@
 export const AppConfig = {
   production: false,
   environment: 'DEV',
-  api: 'http://localhost:3005/',
+  api: 'http://192.168.0.108:3005/',
   tables: ['pensum', 'seccion', 'profesor', 'prelacion', 'materia', 'materia_x_pensum']
 };
