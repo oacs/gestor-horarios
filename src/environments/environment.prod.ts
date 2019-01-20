3d24b44697a7956217bfb2862f0705beb4c95b23
josean0509 <joseanzacarias@gmail.com>
2019-01-20 18:27:31 -0400

    enviroments fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   production: true,
   environment: 'PROD',
-  api: 'http://192.168.0.108:3005/',
+  api: 'http://localhost:3005/',
   tables: ['pensum', 'seccion', 'profesor', 'prelacion', 'materia', 'materia_x_pensum']
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   production: false,
   environment: 'LOCAL',
-  api: 'http://192.168.0.108:3005/',
+  api: 'http://localhost:3005/',
   tables: ['pensum', 'seccion', 'profesor', 'prelacion', 'materia', 'materia_x_pensum']
 };
