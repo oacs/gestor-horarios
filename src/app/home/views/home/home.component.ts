ee809fa561bc3d031f327f1b465a9c0758022284
ntoniocp <antoniojcp96@gmail.com>
2019-01-22 04:23:43 -0400

    notifications (toast) ready

--- src/app/home/views/home/home.component.ts
+++ src/app/home/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationService } from '../../../providers/notificationService/notification.service';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  test = 0;
 
-  constructor() {
+  constructor(private servicioNotificaciones: NotificationService) { }
 
+  testNotif() {
+    this.test++;
+    this.servicioNotificaciones.mostrarNotificacion('Mensaje exitoso numero: ' + this.test);
   }
-
   ngOnInit() {
     /*this.dbService.getProfesores().subscribe( data => {
       console.log(data);
